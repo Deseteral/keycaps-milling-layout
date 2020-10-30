8ac3941881468a7f254a24889ff4dcd3d167e8bf
Deseteral <deseteral@gmail.com>
2020-10-30 21:59:50 +0100

    Refactoring

--- src/code.js
+++ src/code.js
@@ -1,30 +1,45 @@
-function calculate() {
-  // get values from the form
+function getFormValues() {
   const values = {};
+
   ['xCount', 'yCount', 'keycapSize', 'spaceBetween', 'spaceFromX', 'spaceFromY'].forEach((id) => {
     values[id] = parseInt(document.querySelector(`#${id}`).value, 10);
   });
 
-  const { xCount, yCount, keycapSize, spaceBetween, spaceFromX, spaceFromY } = values;
-  const basePosition = document.querySelector('input[name="basePosition"]:checked').value;
+  values.basePosition = document.querySelector('input[name="basePosition"]:checked').value;
 
-  // calculate material size
+  return values;
+}
+
+function calculateMaterial() {
+  const { xCount, yCount, keycapSize, spaceBetween, spaceFromX, spaceFromY } = getFormValues();
+
   const materialWidth = (xCount * keycapSize) + ((xCount - 1) * spaceBetween) + (2 * spaceFromX);
   const materialHeight = (yCount * keycapSize) + ((yCount - 1) * spaceBetween) + (2 * spaceFromY);
 
-  // calculate key positions
-  const keys = [];
-
   const materialCenterX = (materialWidth / 2);
   const materialCenterY = (materialHeight / 2);
+
+  return {
+    width: materialWidth,
+    height: materialHeight,
+    centerX: materialCenterX,
+    centerY: materialCenterY,
+  };
+}
+
+function calculateKeys() {
+  const { xCount, yCount, keycapSize, spaceBetween, spaceFromX, spaceFromY } = getFormValues();
+  const material = calculateMaterial();
+
+  const keys = [];
 
   for (let y = 0; y < yCount; y += 1) {
     for (let x = 0; x < xCount; x += 1) {
       const posX = (spaceFromX + (keycapSize / 2)) + (x * (keycapSize + spaceBetween));
       const posY = (spaceFromY + (keycapSize / 2)) + (y * (keycapSize + spaceBetween));
 
-      const fromCenterX = (posX - materialCenterX);
-      const fromCenterY = (posY - materialCenterY);
+      const fromCenterX = (posX - material.centerX);
+      const fromCenterY = (posY - material.centerY);
 
       keys.push({
         x: posX,
@@ -36,27 +51,34 @@ function calculate() {
     }
   }
 
+  return keys;
+}
+
+function calculate() {
+  const material = calculateMaterial();
+  const keys = calculateKeys();
+  const { basePosition } = getFormValues();
+
   return {
-    materialWidth,
-    materialHeight,
+    material,
     basePosition,
     keys,
   };
 }
 
-function rerender({ materialWidth, materialHeight, basePosition, keys }) {
+function rerender({ material, basePosition, keys }) {
   const RENDER_SCALE = 4;
 
   // clear
   const tileContainer = document.querySelector('#container');
   tileContainer.innerHTML = '';
 
-  // set material size
-  tileContainer.style.width = `${materialWidth * RENDER_SCALE}px`;
-  tileContainer.style.height = `${materialHeight * RENDER_SCALE}px`;
+  // render material
+  tileContainer.style.width = `${material.width * RENDER_SCALE}px`;
+  tileContainer.style.height = `${material.height * RENDER_SCALE}px`;
 
-  document.querySelector('#materialHeight').innerHTML = `${materialWidth} mm`;
-  document.querySelector('#materialWidth').innerHTML = `${materialHeight} mm`;
+  document.querySelector('#materialHeight').innerHTML = `${material.width} mm`;
+  document.querySelector('#materialWidth').innerHTML = `${material.height} mm`;
 
   // render keys
   keys.forEach((key) => {
@@ -66,7 +88,7 @@ function rerender({ materialWidth, materialHeight, basePosition, keys }) {
     el.addEventListener('click', (ev) => ev.target.closest('.tile').classList.toggle('active'));
 
     el.style.left = `${(key.x - (key.size / 2)) * RENDER_SCALE}px`;
-    el.style.top = `${(materialHeight - (key.y - (key.size / 2)) - key.size) * RENDER_SCALE}px`;
+    el.style.top = `${(material.height - (key.y - (key.size / 2)) - key.size) * RENDER_SCALE}px`;
     el.style.width = `${key.size * RENDER_SCALE}px`;
     el.style.height = `${key.size * RENDER_SCALE}px`;
 
