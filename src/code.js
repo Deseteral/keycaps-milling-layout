37e014c882ceae2871b1b2fd74f2cb88821e96ba
Deseteral <deseteral@gmail.com>
2020-10-30 19:28:35 +0100

    Building stuff

--- src/code.js
+++ src/code.js
@@ -1,48 +1,75 @@
-const SCALE = 4;
-
-function generate() {
-  const tileContainer = document.querySelector('#container');
-  tileContainer.innerHTML = '';
-
+function calculate() {
+  // get values from the form
   const values = {};
-  ['xCount', 'yCount', 'keycapSize', 'spaceBetween', 'spaceFromX', 'spaceFromY'].forEach(id => {
+  ['xCount', 'yCount', 'keycapSize', 'spaceBetween', 'spaceFromX', 'spaceFromY'].forEach((id) => {
     values[id] = parseInt(document.querySelector(`#${id}`).value, 10);
   });
 
   const { xCount, yCount, keycapSize, spaceBetween, spaceFromX, spaceFromY } = values;
 
-  // calculate container size
-  const sizeX = (xCount * keycapSize) + ((xCount - 1) * spaceBetween) + (2 * spaceFromX);
-  const sizeY = (yCount * keycapSize) + ((yCount - 1) * spaceBetween) + (2 * spaceFromY);
+  // calculate material size
+  const materialWidth = (xCount * keycapSize) + ((xCount - 1) * spaceBetween) + (2 * spaceFromX);
+  const materialHeight = (yCount * keycapSize) + ((yCount - 1) * spaceBetween) + (2 * spaceFromY);
 
-  tileContainer.style.width = `${sizeX * SCALE}px`;
-  tileContainer.style.height = `${sizeY * SCALE}px`;
+  // calculate key positions
+  const keys = [];
 
-  document.querySelector('#materialHeight').innerHTML = `${sizeX} mm`;
-  document.querySelector('#materialWidth').innerHTML = `${sizeY} mm`;
-
-  // render tiles
-  for (let y = 0; y < yCount; y++) {
-    for (let x = 0; x < xCount; x++) {
+  for (let y = 0; y < yCount; y += 1) {
+    for (let x = 0; x < xCount; x += 1) {
       const posX = (x * (keycapSize + spaceBetween)) + spaceFromX;
       const posY = (y * (keycapSize + spaceBetween)) + spaceFromY;
 
-      const el = document.createElement('div');
-      el.className = 'tile';
-
-      el.addEventListener('click', (ev) => ev.target.closest('.tile').classList.toggle('active'));
-
-      el.style.left = `${posX * SCALE}px`;
-      el.style.top = `${(sizeY - posY - keycapSize) * SCALE}px`;
-      el.style.width = `${keycapSize * SCALE}px`;
-      el.style.height = `${keycapSize * SCALE}px`;
-
-      el.innerHTML = `<div>X ${posX}</div><div>Y ${posY}</div>`;
-
-      tileContainer.appendChild(el);
+      keys.push({
+        x: posX,
+        y: posY,
+        size: keycapSize,
+      });
     }
   }
+
+  return {
+    materialWidth,
+    materialHeight,
+    keys,
+  };
+}
+
+function rerender({ materialWidth, materialHeight, keys }) {
+  const SCALE = 4;
+
+  // clear
+  const tileContainer = document.querySelector('#container');
+  tileContainer.innerHTML = '';
+
+  // set material size
+  tileContainer.style.width = `${materialWidth * SCALE}px`;
+  tileContainer.style.height = `${materialHeight * SCALE}px`;
+
+  document.querySelector('#materialHeight').innerHTML = `${materialWidth} mm`;
+  document.querySelector('#materialWidth').innerHTML = `${materialHeight} mm`;
+
+  // render keys
+  keys.forEach((key) => {
+    const el = document.createElement('div');
+    el.className = 'tile';
+
+    el.addEventListener('click', (ev) => ev.target.closest('.tile').classList.toggle('active'));
+
+    el.style.left = `${key.x * SCALE}px`;
+    el.style.top = `${(materialHeight - key.y - key.size) * SCALE}px`;
+    el.style.width = `${key.size * SCALE}px`;
+    el.style.height = `${key.size * SCALE}px`;
+
+    el.innerHTML = `<div>X ${key.x}</div><div>Y ${key.y}</div>`;
+
+    tileContainer.appendChild(el);
+  });
+}
+
+function generate() {
+  const data = calculate();
+  rerender(data);
 }
 
 document.querySelector('#generate').addEventListener('click', () => generate());
-generate();
+document.addEventListener('DOMContentLoaded', () => generate());
